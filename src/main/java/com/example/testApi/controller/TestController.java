c3519aa647e96210bc6c3d78f3300ef4c41b052e
sai <sai4rall@gmail.com>
2023-02-21 20:26:00 +0530

    second commit

--- src/main/java/com/example/testApi/controller/TestController.java
+++ src/main/java/com/example/testApi/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.testApi.controller;
 
 import com.example.testApi.model.TestInput;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @PostMapping("/add")
     public String test(@RequestBody @Valid TestInput testInput){
-        System.out.println(testInput.getRequestingApplicationName());
+        return "Hello";
+    }
+    @DeleteMapping("/app")
+    public String deleteapp(@RequestBody @Valid TestInput testInput){
         return "Hello";
     }
 }
